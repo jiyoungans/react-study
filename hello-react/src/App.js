4a0fdd598a551d3f01f5c49ce8ef29fccc36e0f9
jiyoungans <jiyoungan00420@gmail.com>
2021-06-25 17:26:57 +0900

    study props, propsType, isRequired on function and class component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent'; // MyComponent 컴포넌트 불러오기
 
 const App = () => {
-    return <MyComponent />
+    return <MyComponent name="이름이름이름" favoriteNumber={3}>칠드런입니다리</MyComponent>
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+// function components
+/*const MyComponent = ({ name, favoritNumber, children }) => {
+    return (
+        <div>
+            <div>안녕하세요, 제 이름은 {name}입니다.</div>
+            <div>children 값은 {children}이죠!</div>
+            <div>제가 가장 좋아하는 숫자는 {favoriteNumber} 입니다.</div>
+        </div>
+    );
 };
+
+MyComponent.defaultProps = {
+    name: '기본값'
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+};*/
+
+class MyComponent extends Component {
+    static defaultProps = { name: '기본이름' };
+
+    static propTypes = {
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired
+    };
+
+    render() {
+        const { name, favoriteNumber, children } = this.props;
+        return (
+            <div>
+                <div>안녕하세요, 제 이름은 {name}입니다.</div>
+                <div>children 값은 {children}이죠!</div>
+                <div>제가 가장 좋아하는 숫자는 {favoriteNumber} 입니다.</div>
+            </div>
+        );
+    }
+}
 
 export default MyComponent; /* 다른 파일에서 이 파일을 import 할 수 있도록 내보내기 설정 */
