cdeab5aaab6c7bbddcf28fbffae5e96092e342b3
jiyoungans <jiyoungan00420@gmail.com>
2021-07-08 18:22:34 +0900

    using state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import MyComponent from './MyComponent'; // MyComponent 컴포넌트 불러오기
+import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
-    return <MyComponent name="이름이름이름" favoriteNumber={3}>칠드런입니다리</MyComponent>
+    return <Say />;
 };
 
 export default App;
---
+++ hello-react/src/Counter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
+
+    render() {
+        const { number, fixedNumber } = this.state;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h1>바뀌지 않는 값 : {fixedNumber}</h1>
+                <button onClick={() => {
+                    this.setState(prevState => {
+                        return {
+                            number: prevState.number + 1
+                        };
+                    });
+
+                    this.setState(prevState => ({
+                        number: prevState.number + 1
+                    }));
+                }}>+1</button>
+                <button onClick={() => {
+                    this.setState({
+                        number: number-1
+                    }, () => {
+                        console.log('callback function has executed.......');
+                    });
+                }}>-1</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
