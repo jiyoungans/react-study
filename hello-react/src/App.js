32d081c1151b10262ebf8aeaf9b2c7d5f7c4882c
jiyoungans <jiyoungan00420@gmail.com>
2021-07-14 16:44:48 +0900

    using ref attribute

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import EventPracticeFunc from './EventPracticeFunc';
+import React, { Component } from 'react';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
+/* const App = () => {
     return <EventPracticeFunc />;
-};
+}; */
+
+class App extends Component {
+    render() {
+        return ( 
+            <div>
+                <ScrollBox ref={(ref) => this.scrollBox = ref} />
+                <button onClick={() => this.scrollBox.scrollToBotton()}>맨 밑으로</button>
+            </div> 
+        );
+    }
+}
 
 export default App;
